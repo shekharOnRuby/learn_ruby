d2aadb5cba9455a50701308ef67d28fdb1df05d1
shekhar karande <shekhar.karande@gmail.com>
2017-11-23 21:44:31 +0000

    submitting first set of successfult tests

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,10 @@
-#write your code here
+def ftoc(temperature)
+	
+	c= ((temperature - 32)/1.8).to_f.ceil
+end
+
+def ctof(temperature)
+
+	f = (32+ temperature*1.8).round(1)
+	
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,16 @@
-#write your code here
+def add(a,b)
+	a+b	
+end
+
+def subtract(large,small)
+	large-small
+end
+
+
+def sum(arr)
+	total_sum=0
+	arr.each do |i|
+		total_sum+=i
+	end
+	total_sum
+end
