77536bb4669d34d687c28c163d99f99e126425e5
shekhar karande <shekhar.karande@gmail.com>
2017-11-29 19:51:24 +0000

    first dab at string

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,19 @@
 #write your code here
+
+def echo(some_string)
+	some_string	
+end
+
+def shout(some_string)
+	some_string.upcase!	
+end
+
+
+def repeat(some_string, iteration=2)
+	iteration.times.collect{some_string}.join(' ')
+end
+
+def start_of_word(some_string, position)
+ 	some_string.slice(0,position)
+end
+
